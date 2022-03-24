791fe7cb18139970ca6ce3b1d4bb82ece8899cf8
Fathul Hidayat <100482319+Kirara02@users.noreply.github.com>
2022-03-24 15:53:50 +0700

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -6,23 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="<?= BASEURL ?>/css/bootstrap.css" rel="stylesheet">
-    <title>Halaman <?= $data['judul']?></title>
+    <title></title>
 </head>
 
 <body>
-    <div class="container">
-        <ul class="nav nav-pills">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="<?= BASEURL ?>/home">PHP MVC</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= BASEURL ?>/home">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= BASEURL ?>/about">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= BASEURL ?>/waifu">Waifu</a>
-            </li>
-        </ul>
-    </div>
